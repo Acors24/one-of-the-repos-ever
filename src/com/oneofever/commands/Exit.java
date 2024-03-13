9b07017887e71c0a2e1e93c257c42fd7858075d2
Michał Łukasik <michal4444333221@gmail.com>
2024-03-13 12:51:34 +0100

    add exit command

---
+++ src/com/oneofever/commands/Exit.java
@@ -0,0 +1,20 @@
+package com.oneofever.commands;
+
+public class Exit implements ICommand {
+
+  @Override
+  public String name() {
+    return "exit";
+  }
+
+  @Override
+  public String description() {
+    return "exit the program";
+  }
+
+  @Override
+  public void run(String[] tokens) {
+    System.exit(0);
+  }
+  
+}
