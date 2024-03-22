9d260abae112409ea75dd023513dcd73a8afcdac
Michał Łukasik <michal4444333221@gmail.com>
2024-03-22 21:54:19 +0100

    removed unused enums

--- src/com/oneofever/shapes/Square.java
+++ src/com/oneofever/shapes/Square.java
@@ -1,12 +1,6 @@
 package com.oneofever.shapes;
 
 public class Square extends Shape {
-    public enum ArgType {
-        Side,
-        Diagonal,
-        Area
-    }
-
     public Square(Properties props) throws IllegalArgumentException
     {
         this.props = props;
@@ -68,39 +62,6 @@ public class Square extends Shape {
             throw new IllegalArgumentException("Too many arguments.");
         }
     }
-
-    // public Square(ArgType type, double value) throws IllegalArgumentException
-    // {
-    //     super();
-    //
-    //     Double side = 0.0;
-    //     Double diagonal = 0.0;
-    //     Double area = 0.0;
-    //
-    //     switch (type)
-    //     {
-    //         case Side:
-    //             side = value;
-    //             diagonal = side * Math.sqrt(2);
-    //             area = side * side;
-    //             break;
-    //
-    //         case Diagonal:
-    //             diagonal = value;
-    //             side = diagonal / Math.sqrt(2);
-    //             area = side * side;
-    //             break;
-    //
-    //         case Area:
-    //             area = value;
-    //             side = Math.sqrt(area);
-    //             diagonal = side * Math.sqrt(2);
-    //             break;
-    //     }
-    //     props.setSides(new Double[]{side});
-    //     props.setDiagonals(new Double[]{diagonal});
-    //     props.setArea(area);
-    // }
 
     public double getSide() {
         return props.getSides()[0];
--- src/com/oneofever/shapes/Triangle.java
+++ src/com/oneofever/shapes/Triangle.java
@@ -1,45 +1,6 @@
 package com.oneofever.shapes;
 
 public class Triangle extends Shape {
-    public enum ArgType {
-        Side,
-        Height,
-        Area
-    }
-
-    public Triangle(ArgType type, double value) throws IllegalArgumentException
-    {
-        super();
-
-        Double side = 0.0;
-        Double height = 0.0;
-        Double area = 0.0;
-
-        switch (type)
-        {
-            case Side:
-                side = value;
-                height = (side * Math.sqrt(3)) / 2;
-                area = (side * height) / 2;
-                break;
-
-            case Height:
-                height = value;
-                side = height * 2 / Math.sqrt(3);
-                area = (side * height) / 2;
-                break;
-
-            case Area:
-                area = value;
-                side = Math.sqrt(area / Math.sqrt(3)) * 2;
-                height = (side * Math.sqrt(3)) / 2;
-                break;
-        }
-        props.setSides(new Double[]{side});
-        props.setHeights(new Double[]{height});
-        props.setArea(area);
-    }
-
     public Triangle(Properties props) throws IllegalArgumentException
     {
         this.props = props;
